59c3ca766c10fe1feba6c8e115d5b936f87fee29
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-09 10:45:43 -0300

    Add files via upload

---
+++ UVVFLIX.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uvvflix;
+
+/**
+ *
+ * @author alunolab10
+ */
+public class UVVFLIX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // CÓDIGO CRUD
+    }
+    //CREAT
+    public void CriarSessao(sessao sessao){
+    }
+    public sessao LerSessao (sessao sessao){
+        return sessao;
+    }
+    
+   public void AtualizarSessao (sessao sessao){
+   }
+   
+   public void ExcluirSessao (sessao sessao){
+   }
+}
---
+++ sala.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uvvflix;
+
+/**
+ *
+ * @author alunolab10
+ */
+public class ator {
+    int idAtor;
+    String nome, papel;
+}
+
