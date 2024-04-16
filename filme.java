d0dd505fa3e59197e3b7a94bdae84d8c57274380
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-16 10:42:51 -0300

    Update filme.java

--- filme.java
+++ filme.java
@@ -8,8 +8,31 @@ package uvvflix;
  *
  * @author alunolab10
  */
-public class ator {
-    int idAtor;
-    String nome, papel;
+public class filme {
+    int idfilme;
+    String titulo, genero;
+    long duracao;
+    
+    
+    public filme (String titulo, String genero, long duracao) {
+    this.titulo = titulo;
+    this.genero = genero;
+    this.duracao = duracao;
 }
-
+    public String gettitulo(){
+        return titulo;
+        System.out.println ("Título: " + filme.gettitulo);
+        
+    }
+    public String getgenero(){
+        return genero;
+        System.out.println ("Gênero: " + filme.getgenero);
+        
+    }
+    
+    public long getduracao(){
+        return duracao;
+        System.out.println ("Duração: " + filme.getduracao);
+    }
+    
+}
