677ac9663d3299cf09d95729215c624f614f0b2d
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-16 10:42:19 -0300

    Update ator.java

--- ator.java
+++ ator.java
@@ -9,7 +9,12 @@ package uvvflix;
  * @author alunolab10
  */
 public class ator {
-    int idAtor;
+     int idAtor;
     String nome, papel;
+    
+    public ator (String nome, String papel){
+    this.nome = nome;
+    this.papel = papel;
 }
 
+}
