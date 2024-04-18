6a7f67bde35616fa923360f8c6b1ee69caa0031f
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-17 22:23:40 -0300

    Update UVVFLIX.java

--- UVVFLIX.java
+++ UVVFLIX.java
@@ -14,18 +14,25 @@ public class UVVFLIX {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // CÓDIGO CRUD
+ 
+    Ator ator1 = new Ator(1, "Timothee Chalamet", "Paul");
+    Ator ator2 = new Ator(2, "Hailee Steinfeld", "Emily Dickinson");
+
+    Filme filme1 = new Filme(1, "Duna", "Ficção Científica", 148);
+    Filme filme2 = new Filme(2, "Interstellar", "Ficção Científica", 169);
+
+   
+    Sala sala1 = new Sala(1, 100, "3D", "Centro");
+    Sala sala2 = new Sala(2, 80, "2D", "Lateral");
+
+   
+    Sessao sessao1 = new Sessao("20:00", filme1, sala1);
+    Sessao sessao2 = new Sessao("22:30", filme2, sala2);
+
+    // Exibindo informações das sessões
+    sessao1.exibirHorario();
+    sessao2.exibirHorario();
+
     }
-    //CREAT
-    public void CriarSessao(sessao sessao){
-    }
-    public sessao LerSessao (sessao sessao){
-        return sessao;
     }
     
-   public void AtualizarSessao (sessao sessao){
-   }
-   
-   public void ExcluirSessao (sessao sessao){
-   }
-}
