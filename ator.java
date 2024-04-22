a4d7a3e5e1b0b12e968df9a47434719d19ab56ae
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-22 09:29:59 -0300

    Update ator.java

--- ator.java
+++ ator.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package uvvflix;
 
 /**
@@ -11,6 +7,7 @@ package uvvflix;
 public class ator {
      int idAtor;
     String nome, papel;
+
     
     public ator (String nome, String papel){
     this.nome = nome;
