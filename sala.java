0395f60f0a9197dc0f578e4986b243be1e9a2d40
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-22 09:30:46 -0300

    Update sala.java

--- sala.java
+++ sala.java
@@ -1,26 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package uvvflix;
-
-/**
- *
- * @author alunolab10
- */
-
 public class sala {
+
     int idSala, numAssentos;
     String tipoTela, localizacao;
+    List<sessao> sessoes = new ArrayList<>();
     
-    public Sala(int idSala, int numAssentos, String tipoTela, String localizacao) {
+
+    public sala(int idSala, int numAssentos, String tipoTela, String localizacao) {
         this.idSala = idSala;
         this.numAssentos = numAssentos;
         this.tipoTela = tipoTela;
         this.localizacao = localizacao;
-            }
+    }
 
-            public void menu (){
+    public void menu() {
+
+    }
+    // Métodos para manipulação da Sessão (CRUD)
+    //Create (cadastrar por profundidade, usar um loop e seguir o fluxo do proprio esquema)
     
-            }
-        }
+}
+    //Read
+    
+    //Update
+    
+    
+    //Delete
+   
