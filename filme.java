3f245fa325c52059100fffa18b4080cd6a32f2bf
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-22 09:30:26 -0300

    Update filme.java

--- filme.java
+++ filme.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package uvvflix;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -12,27 +11,12 @@ public class filme {
     int idfilme;
     String titulo, genero;
     long duracao;
-    
-    
-    public filme (String titulo, String genero, long duracao) {
-    this.titulo = titulo;
-    this.genero = genero;
-    this.duracao = duracao;
-}
-    public String gettitulo(){
-        return titulo;
-        System.out.println ("Título: " + filme.gettitulo);
-        
+    List<ator> atores = new ArrayList<>(); 
+
+    public filme(String titulo, String genero, long duracao) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
     }
-    public String getgenero(){
-        return genero;
-        System.out.println ("Gênero: " + filme.getgenero);
-        
-    }
-    
-    public long getduracao(){
-        return duracao;
-        System.out.println ("Duração: " + filme.getduracao);
-    }
-    
+
 }
