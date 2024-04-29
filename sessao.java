b9ab4745f3852110805bc9a7db634614332d6ff4
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-29 09:29:50 -0300

    Update sessao.java

--- sessao.java
+++ sessao.java
@@ -7,6 +7,7 @@ public class sessao {
     boolean statusSessao;
     String horario;
     List<filme> filmes = new ArrayList<>();
+    List<sessao> sessoes = new ArrayList<>();
 
     
     void cadastrar (filme f){
@@ -27,4 +28,27 @@ public class sessao {
         System.out.println("O horário da sessão é:" + this.horario);
     }
 
+    void cadastrar (sessao se){
+        sessoes.add(se);
+    }
+    
+    void lersessao (sessao se){
+        int index;
+        index = InOut.LeInt(frase:"Escreva o id da sessao que queira ler:");
+        InOut.MsgInformacao(cabecalho:"sala" + ListaSala.get(index-1)); (frase:"horario: "+ this.horario + "\nstatussessao: "+ this.statusSessao);
+    }
+    
+    void removersessao (sessao se){
+        int index;
+        index = IntOut.LeInt(frase:"Digite o Id da sessao que vai ser removida:");
+        Listasala.remove(index: index - 1);
+        InOut.MsgdeInformacao(cabecalho:"sessao", frase: "Removida")
+    }
+    
+    void alterarsessao (sessao se){
+        int index;
+        String var;
+        index = inOut.LeInt(frase:"Digite o id da sessao que vai ser alterada:");
+        
+    }
 }
