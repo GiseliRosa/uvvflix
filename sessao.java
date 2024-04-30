9d6dd4d229bb8b420ea1ba95d1cba5cd253ff8d3
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-29 21:38:06 -0300

    Update sessao.java

--- sessao.java
+++ sessao.java
@@ -19,36 +19,16 @@ public class sessao {
         this.statusSessao = statusSessao;
     }
 
-    public String gethorario() {
+
+     String getStatus() {
+        return status;
+    }
+
+     String getHorario() {
         return horario;
-
     }
 
-    public void exibirhorario() {
-        System.out.println("O horário da sessão é:" + this.horario);
-    }
-
-    void cadastrar (sessao se){
-        sessoes.add(se);
-    }
-    
-    void lersessao (sessao se){
-        int index;
-        index = InOut.LeInt(frase:"Escreva o id da sessao que queira ler:");
-        InOut.MsgInformacao(cabecalho:"sala" + ListaSala.get(index-1)); (frase:"horario: "+ this.horario + "\nstatussessao: "+ this.statusSessao);
-    }
-    
-    void removersessao (sessao se){
-        int index;
-        index = IntOut.LeInt(frase:"Digite o Id da sessao que vai ser removida:");
-        Listasala.remove(index: index - 1);
-        InOut.MsgdeInformacao(cabecalho:"sessao", frase: "Removida")
-    }
-    
-    void alterarsessao (sessao se){
-        int index;
-        String var;
-        index = inOut.LeInt(frase:"Digite o id da sessao que vai ser alterada:");
-        
+     ArrayList<Filme> getFilmes() {
+        return filmes;
     }
 }
