174aebaaf403e22f1e40c41001326dcd15151e23
GiseliRosa <141333387+GiseliRosa@users.noreply.github.com>
2024-04-29 21:38:38 -0300

    Update sala.java

--- sala.java
+++ sala.java
@@ -21,27 +21,23 @@ public class sala {
 
     }
     
-    void cadastrar (sala sa){
-        salas.add(sa);
+    void adicionarSessao(Sessao sessao) {
+        sessoes.add(sessao);
     }
-    
-    void lersala (sala sa){
-        int index;
-        index = InOut.LeInt(frase:"Escreva o id da sala que queira ler:");
-        InOut.MsgInformacao(cabecalho:"sala" + ListaSala.get(index-1)); (frase:"assentos: "+ this.numAssentos + "\ntipoTela: "+ this.tipoTela + "\nlocalização" + this.localizacao + "Id:" + this.idSala);
+
+     int getNumeroAssentos() {
+        return assentos;
     }
-    
-    void removersala (sala sa){
-        int index;
-        index = IntOut.LeInt(frase:"Digite o Id da sala que vai ser removido:");
-        Listasala.remove(index: index - 1);
-        InOut.MsgdeInformacao(cabecalho:"sala", frase: "Removida")
+
+     String getTela() {
+        return tela;
     }
-    
-    void alterarsala (sala sa){
-        int index;
-        String var;
-        index = inOut.LeInt(frase:"Digite o id da sala que vai ser alterada:");
-        
+
+     String getLocal() {
+        return local;
+    }
+
+     ArrayList<Sessao> getSessoes() {
+        return sessoes;
     }
 }
